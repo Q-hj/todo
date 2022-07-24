1108ea06b948fc8bec85cc647e5d0cf1c81a41c8
Qin <1485642186@qq.com>
2022-07-24 14:37:33 +0800

    ☑️

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,33 @@
 /*
  * @Date: 2022-07-22 16:50:58
  * @LastEditors: Mr.qin
- * @LastEditTime: 2022-07-24 13:19:52
+ * @LastEditTime: 2022-07-24 14:36:22
  * @Description: vite 配置文件
  */
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig, loadEnv } from "vite"; // https://vitejs.dev/config/
+
 import vue from "@vitejs/plugin-vue";
+
 import Components from "unplugin-vue-components/vite";
+
 import AutoImport from "unplugin-auto-import/vite";
+
 // import AutoImport from "unplugin-auto-import/vite";
 // import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-// https://vitejs.dev/config/
+
 export default defineConfig({
 	base: "./",
 	plugins: [
 		vue(),
+		//自动导入API
 		AutoImport({
 			// dts: 'src/auto-imports.d.ts', // 可以自定义文件生成的位置，默认是根目录下
-			imports: ["vue"], //自动导入vueAPI
+			imports: ["vue"],
 		}),
 		// AutoImport({
 		// 	resolvers: [ElementPlusResolver()],
 		// }),
+		//自动导入组件
 		Components({
 			// resolvers: [ElementPlusResolver()],
 		}),
